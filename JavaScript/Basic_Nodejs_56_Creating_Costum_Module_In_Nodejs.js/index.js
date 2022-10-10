1d5e46f046562d256fb110fded1e187114edd7b3
SatyajeetBehera1 <86057919+SatyajeetBehera1@users.noreply.github.com>
2022-10-10 18:47:58 +0530

    Create index.js

---
+++ JavaScript/Basic_Nodejs_56_Creating_Costum_Module_In_Nodejs.js/index.js
@@ -0,0 +1,14 @@
+// We are inside index
+
+const mode = require("./Module")
+
+//for single function
+// console.log(mode([1,2,3]))
+// console.log(mode)
+
+//for multiple function
+
+console.log(mode.avg([1,2,3]))
+console.log(mode)
+
+//console.log(mod.name) 
