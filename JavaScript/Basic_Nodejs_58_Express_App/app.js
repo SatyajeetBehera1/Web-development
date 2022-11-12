84f38e696ba9e0d0410420b469aaca004a3a3cfc
SatyajeetBehera1 <86057919+SatyajeetBehera1@users.noreply.github.com>
2022-11-12 19:38:38 +0530

    Create app.js

---
+++ JavaScript/Basic_Nodejs_58_Express_App/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 80;
+ 
+app.get("/", (req, res)=>{ 
+    res.status(200).send("This is homepage of my first express app with Harry");
+});
+
+app.get("/about", (req, res)=>{
+    res.send("This is about page of my first express app with Harry");
+});
+
+app.post("/about", (req, res)=>{
+    res.send("This is a post request about page of my first express app with Harry");
+});
+app.get("/this", (req, res)=>{
+    res.status(404).send("This page is not found on my website cwh");
+});
+
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
